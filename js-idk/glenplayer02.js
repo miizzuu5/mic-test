0771c08b38d2608839b4cb7b77a0b3c12bc4eb01
mizu <miizzuu_5@proton.me>
2026-03-13 21:07:48 +0200

    First Commit

---
+++ js-idk/glenplayer02.js
@@ -0,0 +1,36 @@
+// MUSIC PLAYER BY GLENTHEMES
+
+// you may study the code to see how I did things but please do not steal or claim as your own!
+// learning resource 01: https://www.w3schools.com/jsref/dom_obj_audio.asp
+// learning resource 02: https://www.w3schools.com/tags/tag_audio.asp
+
+// //static.tumblr.com/gtjt4bo/QRmphdsdv/glenplayer02.js
+
+$(document).ready(function(){
+    $(".playy").click(function() {
+        $(".pausee").show();
+        $(".playy").hide();
+    });
+
+    $(".pausee").click(function() {
+        $(".playy").show();
+        $(".pausee").hide();
+    });
+});
+
+function songstart() {
+var harmonia = document.getElementById("tune");
+    if (harmonia.paused) {
+        harmonia.play();
+    } else { 
+        harmonia.pause();
+    }
+}
+
+$(document).ready(function(){
+var owari = document.getElementById("tune");
+owari.onended = function() {
+    $(".playy").show();
+    $(".pausee").hide();
+};
+});
